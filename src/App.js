e334a2545dac742d41f6913f72bc0b286ff2d3bc
拾柒 <1318002479@qq.com>
2021-09-16 10:12:04 +0800

    add

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React,{useState} from 'react';
+import './App.css';
+export default function App() {
+  const [text, setText] = useState("")
+  const handleAdd = ()=>{
+      console.log(text)
+      setlist([...list,text])
+      setText("")
+  }
+  const [list, setlist] = useState([])
+  const handleDel =(index)=>{
+      console.log(index)
+      var newlist = [...list]
+      newlist.splice(index,1)
+      setlist(newlist)
+  }
+  return (
+      <div>     
+          <input type="text" onChange={(evt)=>{
+              setText(evt.target.value)
+          }} value={text}/>
+          <button onClick={handleAdd}>添加</button>
+          {
+              list.map( (item,index)=>
+                  <li key={item}>{item}
+                      <button onClick={()=>handleDel(index)}>删除</button>
+                  </li>    
+              )
+          }
+      </div>
+  )
+}
